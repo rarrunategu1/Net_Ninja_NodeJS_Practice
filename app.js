6730e5a21bf4a0d62a8a749450465235c95a7597
Rose Parker <parker.rose@ymail.com>
2018-09-13 00:06:17 +0000

    Lesson 5 - Function Expressions

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log(__dirname);
+function callFunction(fun){ //this is a function that takes a function as a parameter
+    fun(); //this calls that function
+}
 
-console.log(__filename);
+//function expression
+var sayBye = function(){
+    console.log('bye');
+};
+callFunction(sayBye); //when you call this it uses the sayBye function in the callFunction so when you run it, it returns bye
