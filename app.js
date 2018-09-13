d61bb7439837bde3c20b710c8d80deb2d54a6f44
Rose Parker <parker.rose@ymail.com>
2018-09-13 02:41:14 +0000

    Lesson 12-Clients and Servers

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-var fs = require('fs');  
+var http = require('http');
 
-
-fs.unlink('./stuff/writeMe.txt', function(){ //removes the file first because you can't remove the directly unless it's empty
-fs.rmdir('stuff'); //then after ti removes it it will fire a function to remove the directory as well
+var server = http.createServer(function(req, res){//this creates a server and whenever we send a request to the server, then the function will fire
+    console.log('request was made: ' + req.url); //once you go to the ip address and listen in to the port if you write additional /information on the browser it will console.log it.  This may be good for navigating a website
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey ninjas');
 });
+
+server.listen(3000, '127.0.0.1'); //is the port and IP adddress
+console.log('yo dawgs, now listening to port 3000');
+
+
