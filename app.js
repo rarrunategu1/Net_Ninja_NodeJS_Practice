45fa637ab173ef5ef19fe1fa8104da2ad4552ab6
Rose Parker <parker.rose@ymail.com>
2018-09-13 01:21:49 +0000

    Lesson 8 - The event module

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-var stuff = require('./stuff');
+var events = require('events');
+var util = require('util');
 
-console.log(stuff.counter(['shaun', 'crystal', 'ryu']));
-console.log(stuff.adder(5,6));
-console.log(stuff.adder(stuff.pi, 5));
+var Person = function(name){ //person is an object constructor
+    this.name = name;
+};
+
+util.inherits(Person, events.EventEmitter) //this allows us to make the Person variable inherit the eventemitter so that we can attach custom events to certain people.
+
+var james = new Person('james');
+var mary = new Person('mary');
+var rose = new Person('rose');
+
+var people = [james, mary, rose];
+
+people.forEach(function(person){
+    person.on('speak', function(mssg){
+       console.log(person.name + ' said: ' + mssg); 
+    });
+});
+
+james.emit('speak', 'hey dudes');
