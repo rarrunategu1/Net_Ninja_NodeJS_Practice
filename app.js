eba1e2ff512ab6787e24eb722f07a9c6f06c7651
Rose Parker <parker.rose@ymail.com>
2018-09-15 17:34:13 +0000

    Lesson 25 - Templating Engines- html send back and dynamic inject with ejs

--- app.js
+++ app.js
@@ -2,16 +2,18 @@ var express = require('express');
 
 var app = express(); //this will allow us to fire the express function using the variable app
 
-app.get('/', function(req, res){ //express has added these functions and added functionalities
-  res.send('this is the homepage');
+app.set('view engine', 'ejs'); 
+app.get('/', function(req, res){
+res.sendFile(__dirname + '/index.html');
 });
-app.get('/contact', function(req, res){ //express has added these functions and added functionalities
-  res.send('this is the contact page');
+app.get('/contact', function(req, res){ 
+res.sendFile(__dirname + '/contact.html');
 });
 
-app.get('/profile/:id', function(req, res){ //if you change the :id to :name you just change it to req.params.name on following line
-  res.send('You requested to see a profile with the id of ' + req.params.id); //req.params.id = is asking for the parameter id in the request
-})
+app.get('/profile/:name', function(req, res){
+var data = {age: 29, job: 'ninja'};
+res.render('viewsprofiles', {person: req.params.name, data: data}); //render basically renders a view to the viewsprofiles.ejs file
+}); //pass data to a view by passing object as a 2nd parameter which is number
 
 app.listen(process.env.PORT, process.env.IP, 3000);
 
