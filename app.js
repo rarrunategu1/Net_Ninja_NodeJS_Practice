c45e737b24a88832c8d5bdfc470e5973a18a1279
Rose Parker <parker.rose@ymail.com>
2018-09-15 16:15:48 +0000

    Lesson 22 - Installing Nodemon

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var server = http.createServer(function(req, res){//this creates a server and wh
    if(req.url === '/home' || req.url === '/'){
      res.writeHead(200, {'Content-type': 'text/html'});
      fs.createReadStream(__dirname + '/index.html').pipe(res);
-   } else if (req.url === '/contact'){
+   } else if (req.url === '/contact/us'){
       res.writeHead(200, {'Content-type': 'text/html'});
      fs.createReadStream(__dirname + '/contact.html').pipe(res);
    } else if (req.url === '/api/ninjas'){
