7160131e12568ec2094489da3f0bee5b38e50ebb
Rose Parker <parker.rose@ymail.com>
2018-09-15 02:10:40 +0000

    Lesson 17-Serving HTML

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){//this creates a server and whenever we send a request to the server, then the function will fire
     console.log('request was made: ' + req.url); //once you go to the ip address and listen in to the port if you write additional /information on the browser it will console.log it.  This may be good for navigating a website
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var myReadStream = fs.createReadStream(__dirname + '/readme.txt', 'utf8'); // This reads the file
+    res.writeHead(200, {'Content-Type': 'text/html'}); //use html and it will render it in the browser properly
+    var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8'); // This reads the file
     myReadStream.pipe(res);
     
 });
