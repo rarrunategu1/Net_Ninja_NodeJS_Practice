4a740d370b30f7beaf535cf505a2f8f9889a0f0b
Rose Parker <parker.rose@ymail.com>
2018-09-18 22:52:00 +0000

    Lesson 28 - adding middleware with express to see static files on linked to ejs files

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 var express = require('express');
 
-var app = express(); //this will allow us to fire the express function using the variable app
-
+var app = express(); 
 app.set('view engine', 'ejs'); 
+app.use('/assets', express.static('assets')); //express.static is the middleware that express comes with. 'assets' is the folder name
+  
+
 app.get('/', function(req, res){
 res.render('index');
 });
 app.get('/contact', function(req, res){ 
-res.render('contact');  //this knows where to look so it will pull the contact.ejs file
-});
+res.render('contact');  });
 
 app.get('/profile/:name', function(req, res){
 var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
 res.render('viewsprofiles', {person: req.params.name, data: data}); //render basically renders a view to the viewsprofiles.ejs file
-}); //pass data to a view by passing object as a 2nd parameter which is number
-
+}); 
 app.listen(process.env.PORT, process.env.IP);
 
 
