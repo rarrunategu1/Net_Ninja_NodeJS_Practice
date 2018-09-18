08e91832475d1df5e76ce1d230b4aa767b6bf760
Rose Parker <parker.rose@ymail.com>
2018-09-18 23:41:55 +0000

    Lesson 29 - accessing query strings in a url and rendering it in a view

--- app.js
+++ app.js
@@ -9,11 +9,13 @@ app.get('/', function(req, res){
 res.render('index');
 });
 app.get('/contact', function(req, res){ 
-res.render('contact');  });
+res.render('contact', {qs: req.query});  
+    
+}); //you can now return the query in the view by rendering it in the view below
 
 app.get('/profile/:name', function(req, res){
 var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
-res.render('viewsprofiles', {person: req.params.name, data: data}); //render basically renders a view to the viewsprofiles.ejs file
+res.render('viewsprofiles', {person: req.params.name, data: data}); //here we've rendered the view to the query
 }); 
 app.listen(process.env.PORT, process.env.IP);
 
