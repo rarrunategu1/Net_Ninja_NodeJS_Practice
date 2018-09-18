a38130c9ba7c5d8d4c92872e15430c0b406dd7fc
Rose Parker <parker.rose@ymail.com>
2018-09-18 01:26:13 +0000

    Lesson 27 Partial Views - using EJS files instead of html

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ var app = express(); //this will allow us to fire the express function using the
 
 app.set('view engine', 'ejs'); 
 app.get('/', function(req, res){
-res.sendFile(__dirname + '/index.html');
+res.render('index');
 });
 app.get('/contact', function(req, res){ 
-res.sendFile(__dirname + '/contact.html');
+res.render('contact');  //this knows where to look so it will pull the contact.ejs file
 });
 
 app.get('/profile/:name', function(req, res){
@@ -15,7 +15,7 @@ var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
 res.render('viewsprofiles', {person: req.params.name, data: data}); //render basically renders a view to the viewsprofiles.ejs file
 }); //pass data to a view by passing object as a 2nd parameter which is number
 
-app.listen(process.env.PORT, process.env.IP, 3000);
+app.listen(process.env.PORT, process.env.IP);
 
 
 
