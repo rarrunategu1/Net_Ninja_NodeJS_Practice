c03bcc64a5102a3267fb8d2b2654677b58e62878
Rose Parker <parker.rose@ymail.com>
2018-09-19 00:22:35 +0000

    Lesson 30 - Post requests to post information entered by user on the website in a particular url view

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser'); //this was given by the body-parser information on the npm website
 
 var app = express(); 
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false }); //this is the function we want to invoke in our middleware to parse our POST data
 app.set('view engine', 'ejs'); 
 app.use('/assets', express.static('assets')); //express.static is the middleware that express comes with. 'assets' is the folder name
   
@@ -12,7 +15,11 @@ app.get('/contact', function(req, res){
 res.render('contact', {qs: req.query});  
     
 }); //you can now return the query in the view by rendering it in the view below
-
+app.post('/contact', urlencodedParser, function(req, res){ 
+console.log(req.body);
+res.render('contact-success', {data: req.body}); //this will now give us access to the data in the form in the contact view 
+    
+});
 app.get('/profile/:name', function(req, res){
 var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
 res.render('viewsprofiles', {person: req.params.name, data: data}); //here we've rendered the view to the query
