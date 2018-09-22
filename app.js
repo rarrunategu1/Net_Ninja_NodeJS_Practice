5505b6a2c747770db25120704ba79743700eb4c1
Rose Parker <parker.rose@ymail.com>
2018-09-22 14:33:54 +0000

    Lesson 32-started files and framework for the todolist

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
 var express = require('express');
-var bodyParser = require('body-parser'); //this was given by the body-parser information on the npm website
+var todoController = require('./controllers/todocontroller'); //to include todocontroller.js module
+var app = express();
 
-var app = express(); 
+//set up template engine
+app.set('view engine', 'ejs');
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false }); //this is the function we want to invoke in our middleware to parse our POST data
-app.set('view engine', 'ejs'); 
-app.use('/assets', express.static('assets')); //express.static is the middleware that express comes with. 'assets' is the folder name
-  
+//static files
+app.use(express.static('./public')); //this will be used on every route we put into the url bar
 
-app.get('/', function(req, res){
-res.render('index');
-});
-app.get('/contact', function(req, res){ 
-res.render('contact', {qs: req.query});  
-    
-}); //you can now return the query in the view by rendering it in the view below
-app.post('/contact', urlencodedParser, function(req, res){ 
-console.log(req.body);
-res.render('contact-success', {data: req.body}); //this will now give us access to the data in the form in the contact view 
-    
-});
-app.get('/profile/:name', function(req, res){
-var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
-res.render('viewsprofiles', {person: req.params.name, data: data}); //here we've rendered the view to the query
-}); 
+
+//fire controllers
+todoController(app); //remember app is used in the todoController.js file
+
+
+//listen to port
 app.listen(process.env.PORT, process.env.IP);
-
+console.log('You are listening to port 3000');
 
 
---
+++ controllers/todocontroller.js
@@ -0,0 +1,19 @@
+//This file will control behavior of todolist by manipulating data and handle route. etc
+
+module.exports = function(app){ //app is the variable to run the express package in app.js
+
+//make handlers for different requests we're going to get
+
+app.get('/todo', function(req, res){ //get request for the url itself
+    
+});
+
+app.post('/todo', function(req, res){ //get request for the url itself
+    
+});
+
+//for user to delete 
+app.delete('/todo', function(req, res){
+    
+});
+};
